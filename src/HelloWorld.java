28d6afa6bbf16502571dfde62be5e981fc725609
hanbinc <hanbinc@yonyou.com>
2014-04-21 13:38:06 +0800

    xx
    Change-Id: Ia093eb67eb80d69d8f4caf73efec7baaa0ee578e

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ *
+ * <code>HelloWorld<code>
+ * <strong></strong>
+ * <p>说明：
+ * <li></li>
+ * </p>
+ * @since NC6.31
+ * @version 2014-3-1 上午10:51:56
+ * @author hanbinc
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(new Integer(1).equals(1));
+     }
+}
